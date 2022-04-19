37725de74c1c847fc4f251c9bb99f776de235a05
Gleb <37244009+glebbb@users.noreply.github.com>
2022-04-19 11:52:52 +0300

    Update script.js

--- calculator/calc/js/script.js
+++ calculator/calc/js/script.js
@@ -1,3 +1,4 @@
+
 let buttonClear = document.getElementById('clear_btn');
 let finalCalculation = document.getElementById('calculation');
 let buttonCount = document.getElementById('count_btn');
@@ -16,6 +17,9 @@ buttonClear.addEventListener('click', ()=>{
     finalValues.innerHTML = "";
 })
 
+buttonCount.addEventListener('click', ()=>{
+    calculation();
+})
 
 
 option1.value = `${localStorage.getItem('bank name 1')}`;
@@ -51,70 +55,66 @@ selectBtn.addEventListener('click', ()=>{
         months.value = `${localStorage.getItem(`bank months 3`)}`;
     }
 
-    console.log(data);
-
-    buttonCount.addEventListener('click', ()=>{
-        calculation();
-    })
-
-    function calculation(){
-        let amount = document.querySelector('.amount').value;
-        let percent = document.querySelector('.percent').value;
-        let months = document.querySelector('.months').value;
-    
-        let firstMessage = `<div class = 'first_message'>Сумма займа: <strong>${amount}$</strong><br>Годовой процент: <strong>${percent}%</strong><br>Срок займа <strong>${months} месяц(ев).</strong><br></div>`;
-       
-        let monthPercent = Number(0);
-    
-        finalValues.innerHTML = firstMessage;
-        
-        amount = Number(amount);
-    
-        let finalValue = Number(0);
-        let payment = Number(0);
-        
-    
-        for(let i = 1; i <= months; i++){
-            
-            monthPercent = amount*((percent/12)/100);
-            
-            finalValue = (monthPercent*100)/100;
-            
-            payment = payment+finalValue;   
-        
-        }
-    
-        payment = payment + amount;
-        
-        payment = payment.toFixed(2);
-        finalValue = finalValue.toFixed(2);
-    
-    
-        monthlyPayment.innerHTML = `<br><span class = 'monthly'>Ежемесячный платёж: <span class = 'payment_num'>${finalValue}$</span></span>`;
-            
-        finalCalculation.innerHTML = `<br><span class = 'final_value'>Итого выплатить: <span class = 'calculation_num'>${payment}$</span></span>`; 
-    
-        if ((data === `${localStorage.getItem('bank name 1')}`) && (amount < `${localStorage.getItem('bank min predoplata 1')}` || amount > `${localStorage.getItem('bank max credit 1')}`)){
-            amount = '';
-            finalCalculation.innerHTML = "";
-            monthlyPayment.innerHTML = "";
-            finalValues.innerHTML = "";  
-            alert(`Сумма кредита не должна быть меньше минимальной предоплаты или выше максимального кредита! Для этого банка такие значения: Минимальная предоплата = ${localStorage.getItem('bank min predoplata 1')}. Максимальный кредит = ${localStorage.getItem('bank max credit 1')}.`);
-        } else if ((data === `${localStorage.getItem('bank name 2')}`) && (amount < `${localStorage.getItem('bank min predoplata 2')}` || amount > `${localStorage.getItem('bank max credit 2')}`)){
-            amount = '';
-            finalCalculation.innerHTML = "";
-            monthlyPayment.innerHTML = "";
-            finalValues.innerHTML = "";  
-            alert(`Сумма кредита не должна быть меньше минимальной предоплаты или выше максимального кредита! Для этого банка такие значения: Минимальная предоплата = ${localStorage.getItem('bank min predoplata 2')}. Максимальный кредит = ${localStorage.getItem('bank max credit 2')}.`);
-        } else if ((data === `${localStorage.getItem('bank name 3')}`) && (amount < `${localStorage.getItem('bank min predoplata 3')}` || amount > `${localStorage.getItem('bank max credit 3')}`)){
-            
-            amount = '';
-            finalCalculation.innerHTML = "";
-            monthlyPayment.innerHTML = "";
-            finalValues.innerHTML = "";  
-            alert(`Сумма кредита не должна быть меньше минимальной предоплаты или выше максимального кредита! Для этого банка такие значения: Минимальная предоплата = ${localStorage.getItem('bank min predoplata 3')}. Максимальный кредит = ${localStorage.getItem('bank max credit 3')}.`);
-        }    
-        data = "";
-    }
     
 })
+
+function calculation(){
+    let data = select.value;
+    let amount = document.querySelector('.amount').value;
+    let percent = document.querySelector('.percent').value;
+    let months = document.querySelector('.months').value;
+
+    let firstMessage = `<div class = 'first_message'>Сумма займа: <strong>${amount}$</strong><br>Годовой процент: <strong>${percent}%</strong><br>Срок займа <strong>${months} месяц(ев).</strong><br></div>`;
+   
+    let monthPercent = Number(0);
+
+    finalValues.innerHTML = firstMessage;
+    
+    amount = Number(amount);
+
+    let finalValue = Number(0);
+    let payment = Number(0);
+    
+
+    for(let i = 1; i <= months; i++){
+        
+        monthPercent = amount*((percent/12)/100);
+        
+        finalValue = (monthPercent*100)/100;
+        
+        payment = payment+finalValue;   
+    
+    }
+
+    payment = payment + amount;
+    
+    payment = payment.toFixed(2);
+    finalValue = finalValue.toFixed(2);
+
+
+    monthlyPayment.innerHTML = `<br><span class = 'monthly'>Ежемесячный платёж: <span class = 'payment_num'>${finalValue}$</span></span>`;
+        
+    finalCalculation.innerHTML = `<br><span class = 'final_value'>Итого выплатить: <span class = 'calculation_num'>${payment}$</span></span>`; 
+
+    if ((data === `${localStorage.getItem('bank name 1')}`) && (amount < `${localStorage.getItem('bank min predoplata 1')}` || amount > `${localStorage.getItem('bank max credit 1')}`)){
+        amount = '';
+        finalCalculation.innerHTML = "";
+        monthlyPayment.innerHTML = "";
+        finalValues.innerHTML = "";  
+        alert(`Сумма кредита не должна быть меньше минимальной предоплаты или выше максимального кредита! Для этого банка такие значения: Минимальная предоплата = ${localStorage.getItem('bank min predoplata 1')}. Максимальный кредит = ${localStorage.getItem('bank max credit 1')}.`);
+    } else if ((data === `${localStorage.getItem('bank name 2')}`) && (amount < `${localStorage.getItem('bank min predoplata 2')}` || amount > `${localStorage.getItem('bank max credit 2')}`)){
+        amount = '';
+        finalCalculation.innerHTML = "";
+        monthlyPayment.innerHTML = "";
+        finalValues.innerHTML = "";  
+        alert(`Сумма кредита не должна быть меньше минимальной предоплаты или выше максимального кредита! Для этого банка такие значения: Минимальная предоплата = ${localStorage.getItem('bank min predoplata 2')}. Максимальный кредит = ${localStorage.getItem('bank max credit 2')}.`);
+    } else if ((data === `${localStorage.getItem('bank name 3')}`) && (amount < `${localStorage.getItem('bank min predoplata 3')}` || amount > `${localStorage.getItem('bank max credit 3')}`)){
+        
+        amount = '';
+        finalCalculation.innerHTML = "";
+        monthlyPayment.innerHTML = "";
+        finalValues.innerHTML = "";  
+        alert(`Сумма кредита не должна быть меньше минимальной предоплаты или выше максимального кредита! Для этого банка такие значения: Минимальная предоплата = ${localStorage.getItem('bank min predoplata 3')}. Максимальный кредит = ${localStorage.getItem('bank max credit 3')}.`);
+    }    
+    data = "";
+}
