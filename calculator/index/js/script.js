e59311c10730d11d437f175b6672c3ef2c8da526
Gleb <37244009+glebbb@users.noreply.github.com>
2022-04-22 09:01:29 +0300

    Add files via upload

---
+++ calculator/index/js/script.js
@@ -0,0 +1,165 @@
+
+const openPopUp = document.querySelector('.open_pop_up');
+const closePopUp = document.querySelector('.pop_up_btn');
+const popUp = document.querySelector('.pop_up');
+const bankCreate = document.querySelector('.pop_up_create');
+const newBank1 = document.querySelector('.new_bank_1');
+const newBank2 = document.querySelector('.new_bank_2');
+const newBank3 = document.querySelector('.new_bank_3');
+const deleteBtn1 = document.querySelector('.button_delete_1');
+const deleteBtn2 = document.querySelector('.button_delete_2');
+const deleteBtn3 = document.querySelector('.button_delete_3');
+
+console.log(bankCreate);
+
+let newBankCreate;
+let i = 0;
+
+window.onload = function(){
+
+    newBank1.classList.add('inactive_1');
+    newBank2.classList.add('inactive_2');
+    newBank3.classList.add('inactive_3');
+  
+}
+
+openPopUp.addEventListener('click', (e)=>{
+    e.preventDefault();
+    popUp.classList.add('active');
+})
+
+closePopUp.addEventListener('click', () => {
+    confirm('Все несохранённые данные будут утеряны.');
+    popUp.classList.remove('active');
+})
+
+bankCreate.addEventListener('click', () => { 
+    popUp.classList.remove('active');
+    addNewBank();   
+})
+
+function addNewBank(){    
+    
+    let bankName = document.querySelector('.bank_name').value;
+    let bankProc = document.querySelector('.bank_proc').value;
+    let bankCred = document.querySelector('.max_cred').value;
+    let bankPred = document.querySelector('.max_pred').value;
+    let bankTerm = document.querySelector('.max_term').value;  
+
+    i++;
+
+    let newBankName = document.createElement('div');
+    newBankName.innerHTML = (`<p class = 'title_bank'>${bankName}</p>`);
+    
+    let newBankProc = document.createElement('div');
+    newBankProc.innerHTML = (`<p><span class = 'bank_span'>Процентная ставка</span>: ${bankProc}%</p>`);
+    
+    let newBankCred = document.createElement('div');
+    newBankCred.innerHTML = (`<p><span class = 'bank_span'>Максимальный кредит</span>: ${bankCred}$</p>`);
+   
+    let newBankPred = document.createElement('div');
+    newBankPred.innerHTML = (`<p><span class = 'bank_span'>Минимальная предоплата</span>: ${bankPred}%</p>`);
+   
+    let newBankTerm = document.createElement('div');
+    newBankTerm.innerHTML = (`<p><span class = 'bank_span'>Срок кредита</span>: ${bankTerm} month(s)</p>`);
+
+
+    localStorage.setItem(`bank name ${i}`, bankName);
+    localStorage.setItem(`bank procent ${i}`, bankProc);
+    localStorage.setItem(`bank max credit ${i}`, bankCred);
+    localStorage.setItem(`bank min predoplata ${i}`, bankPred);
+    localStorage.setItem(`bank months ${i}`, bankTerm);
+
+
+    removeStyles();
+
+
+
+    newBankCreate = document.querySelector(`.new_bank_${i}`);
+
+    console.log(newBankCreate);
+
+
+
+     
+    
+    newBankCreate.append(newBankName);
+    newBankCreate.append(newBankProc);
+    newBankCreate.append(newBankCred);
+    newBankCreate.append(newBankPred);
+    newBankCreate.append(newBankTerm);
+
+    deleteBtn1.addEventListener('click', ()=>{
+        let isDelete = confirm(`Вы действительно хотите удалить этот банк?`)
+        if(isDelete == true){
+            newBank1.classList.add('inactive_1');
+            localStorage.clear();
+        }
+    })
+
+    deleteBtn2.addEventListener('click', ()=>{ 
+        let isDelete = confirm(`Вы действительно хотите удалить этот банк?`)
+        if(isDelete == true){
+            newBank2.classList.add('inactive_2');
+            localStorage.clear();
+        }
+    })
+
+    deleteBtn3.addEventListener('click', ()=>{
+        let isDelete = confirm(`Вы действительно хотите удалить этот банк?`)
+        if(isDelete == true){
+            newBank3.classList.add('inactive_3');
+            localStorage.clear();
+        }
+    })
+
+
+
+    
+
+
+}
+
+function removeStyles(){
+    if (i == 1){
+        newBank1.classList.remove('inactive_1');
+    } else if (i == 2){
+        newBank2.classList.remove('inactive_2');
+    } else if (i == 3){
+        newBank3.classList.remove('inactive_3');
+    } else {
+        alert('Вы создали максимальное количество банков.');
+        popUp.classList.remove('active');
+    }
+}
+
+
+
+
+function letterLimit (){  
+    let bankName = document.querySelector('.bank_name');
+    let bankProc = document.querySelector('.bank_proc');
+    let bankCred = document.querySelector('.max_cred');
+    let bankPred = document.querySelector('.max_pred');
+    let bankTerm = document.querySelector('.max_term');
+
+
+    bankName.addEventListener('mouseout', ()=>{
+        bankName.value = bankName.value.substr(0, 10);
+    })
+    bankProc.addEventListener('mouseout', ()=>{
+        bankProc.value = bankProc.value.substr(0, 3);
+    })
+    bankCred.addEventListener('mouseout', ()=>{
+        bankCred.value = bankCred.value.substr(0, 7);
+    })
+    bankPred.addEventListener('mouseout', ()=>{
+        bankPred.value = bankPred.value.substr(0, 3);
+    })
+    bankTerm.addEventListener('mouseout', ()=>{
+        bankTerm.value = bankTerm.value.substr(0, 3);
+    })
+    
+}
+
+letterLimit();
